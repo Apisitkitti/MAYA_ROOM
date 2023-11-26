a61501a3798cd78d626f740f1a661c0db8c34f40
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2023-11-27 03:46:05 +0700

    install commit

---
+++ Assets/move.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class move : MonoBehaviour
+{
+   Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+          anim.SetBool("walk",true);
+          anim.SetBool("walk",false);
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+          anim.SetBool("walk",false);
+          anim.SetBool("walk",true);
+        }
+    }
+}
